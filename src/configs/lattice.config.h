56c4c717f2c92f5f668ac6d529ef17f9b91f6be6
Ambroise Muller <ambroisemuller3@gmail.com>
2024-02-14 13:50:24 +0100

    fix merge

--- src/configs/lattice.config.h
+++
@@ -1,4 +0,0 @@
-#define     D           2               // dimension (fixed = 2)
-#define     Nt          48               // temporal extent
-#define     Nx          48               // spatial extent
-#define     spacing     1.0             // lattice spacing
